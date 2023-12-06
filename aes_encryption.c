72dfbeb2775ec1987726443d9875a5abf7ceecf8
void110916 <110916fong@gmail.com>
2023-12-06 16:45:50 +0800

    Impliment basic aes encode function

--- aes_encryption.c
+++ aes_encryption.c
@@ -2,6 +2,17 @@
 #include <stdint.h>
 #include <string.h>
 
+#if defined(AES256) && (AES256 == 1)
+    int Nk= 8;
+    int Nr= 14;
+#elif defined(AES192) && (AES192 == 1)
+    int Nk= 6;
+    int Nr= 12;
+#else
+    int Nk= 4;        // The number of 32 bit words in a key.
+    int Nr= 10;       // The number of rounds in AES Cipher.
+#endif
+
 typedef uint8_t stat_t[4*4];
 
 const uint8_t subTable [16*16]={
@@ -107,7 +118,7 @@ void subBytes(stat_t *stat){
     }
 }
 
-void shiftBytes(stat_t* stat){
+void shiftRows(stat_t* stat){
     uint32_t *row;
     #pragma omp parallel for
     for (int i = 0; i < 3; i++){
@@ -133,4 +144,95 @@ void addRoundKey(stat_t *stat,const uint8_t* roundkey,uint8_t round){
     {
         (*stat)[i]^=roundkey[i+round*4*4];
     }
+}
+
+static const uint8_t Rcon[11] = {
+  0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36 };
+
+void keyExpain(const uint8_t *key,uint8_t *roundKey){
+    uint8_t tempa[4];
+    int j,k;
+    for (int i = 0; i < 4; i++)
+    {
+        roundKey[(i * 4) + 0] = key[(i * 4) + 0];
+        roundKey[(i * 4) + 1] = key[(i * 4) + 1];
+        roundKey[(i * 4) + 2] = key[(i * 4) + 2];
+        roundKey[(i * 4) + 3] = key[(i * 4) + 3];
+    }
+    
+    for (int i = 4; i < 44; i++)
+    {
+        {
+            k = (i - 1) * 4;
+            tempa[0]=roundKey[k + 0];
+            tempa[1]=roundKey[k + 1];
+            tempa[2]=roundKey[k + 2];
+            tempa[3]=roundKey[k + 3];
+        }
+
+        if (i % 4 == 0)
+        {
+        // This function shifts the 4 bytes in a word to the left once.
+        // [a0,a1,a2,a3] becomes [a1,a2,a3,a0]
+
+        // Function RotWord()
+        {
+            uint32_t *row;
+            // const uint8_t u8tmp = tempa[0];
+            // tempa[0] = tempa[1];
+            // tempa[1] = tempa[2];
+            // tempa[2] = tempa[3];
+            // tempa[3] = u8tmp;
+
+            row=tempa;
+            (*row)=(*row)<<(1<<3)|(*row)>>((4-1)<<3);
+        }
+
+        // SubWord() is a function that takes a four-byte input word and 
+        // applies the S-box to each of the four bytes to produce an output word.
+
+        // Function Subword()
+        {
+            tempa[0] = subTable[tempa[0]];
+            tempa[1] = subTable[tempa[1]];
+            tempa[2] = subTable[tempa[2]];
+            tempa[3] = subTable[tempa[3]];
+        }
+
+        tempa[0] = tempa[0] ^ Rcon[i/4];
+        }
+#if defined(AES256) && (AES256 == 1)
+        if (i % Nk == 4)
+        {
+        // Function Subword()
+        {
+            tempa[0] = subTable[tempa[0]];
+            tempa[1] = subTable[tempa[1]];
+            tempa[2] = subTable[tempa[2]];
+            tempa[3] = subTable[tempa[3]];
+        }
+        }
+#endif
+        j = i * 4; k=(i - 4) * 4;
+        roundKey[j + 0] = roundKey[k + 0] ^ tempa[0];
+        roundKey[j + 1] = roundKey[k + 1] ^ tempa[1];
+        roundKey[j + 2] = roundKey[k + 2] ^ tempa[2];
+        roundKey[j + 3] = roundKey[k + 3] ^ tempa[3];
+    }
+    
+    
+}
+
+void aes_encode(stat_t *stat,const uint8_t *roundKey){
+    addRoundKey(stat,roundKey,0);
+    for (int round = 1;; round++)
+    {
+        subBytes(stat);      
+        shiftRows(stat);     
+        if(round==Nr)
+            break;
+        mixColumns(stat);    
+        addRoundKey(stat,roundKey,round);
+    }
+    addRoundKey(stat,roundKey,Nr);
 }
